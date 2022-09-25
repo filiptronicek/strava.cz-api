bc0715d327730f349c106a48a21d992a1b8c8b80
Filip Troníček <filip@gitpod.io>
2022-09-25 15:49:16 +0000

    Headers
    Fixes #1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/api/*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  },
 }
